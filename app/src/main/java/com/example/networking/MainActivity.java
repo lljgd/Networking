825814e9b94898d141b14147d528775fc4e1c73e
Javernaut <javernaut@gmail.com>
2019-12-20 19:47:28 +0200

    Adding an AsyncTask for simple threading

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.networking;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.example.networking.adapter.GithubersAdapter;
 import com.example.networking.model.Githuber;
 
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,9 +25,29 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        new InternetRequestTask().execute();
     }
 
+    @MainThread
     private void applyGithubers(List<Githuber> githubers) {
         recyclerView.setAdapter(new GithubersAdapter(githubers));
+    }
+
+    private List<Githuber> executeRequest() {
+        return Collections.emptyList();
+    }
+
+    private class InternetRequestTask extends AsyncTask<Void, Void, List<Githuber>> {
+
+        @Override
+        protected List<Githuber> doInBackground(Void... voids) {
+            return executeRequest();
+        }
+
+        @Override
+        protected void onPostExecute(List<Githuber> githubers) {
+            applyGithubers(githubers);
+        }
     }
 }
