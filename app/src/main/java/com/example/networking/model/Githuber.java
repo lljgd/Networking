7e11eee599defbe18e3818661ee1426116a1c1b6
Javernaut <javernaut@gmail.com>
2019-12-21 11:52:19 +0200

    Using OkHttp

--- app/src/main/java/com/example/networking/model/Githuber.java
+++ app/src/main/java/com/example/networking/model/Githuber.java
@@ -1,8 +1,14 @@
 package com.example.networking.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Githuber {
+
     private long id;
+
     private String login;
+
+    @SerializedName("avatar_url")
     private String avatarUrl;
 
     public long getId() {
--- app/src/main/java/com/example/networking/networking/okhttp/NetworkingWithOkHttp.java
+++ app/src/main/java/com/example/networking/networking/okhttp/NetworkingWithOkHttp.java
@@ -1,12 +1,49 @@
 package com.example.networking.networking.okhttp;
 
-import com.example.networking.model.Githuber;
+import android.util.Log;
 
+import com.example.networking.model.Githuber;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class NetworkingWithOkHttp {
     public static List<Githuber> makeRequest() {
-        return Collections.emptyList();
+        // Request
+        Request request = new Request.Builder()
+                .url("https://api.github.com/users")
+                .build();
+
+        // Execute request
+
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+
+        String stringResponse = null;
+
+        try {
+            Response response = call.execute();
+            stringResponse = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.e("asdfsa", stringResponse);
+
+        // Parse response
+
+        Type crimesListType = new TypeToken<List<Githuber>>(){}.getType();
+        List<Githuber> result = new Gson().fromJson(stringResponse, crimesListType);
+
+        return result;
     }
 }
